3c37418974745deae6f71d4fdd6f0f184cfcd913
Pawel <pablogrosinsi@gmail.com>
2019-04-12 23:30:51 +0200

    a

--- login.php
+++ login.php
@@ -3,18 +3,18 @@
  $password = filter_input(INPUT_POST, 'password');
  if (!empty($username)){
 if (!empty($password)){
-$host = "sql2.freesqldatabase.com";
-$dbusername = "sql2287835";
-$dbpassword = "bV9!gQ8*";
-$dbname = "sql2287835";
+$host = 'sql2.freesqldatabase.com';
+$dbusername = 'sql2287835';
+$dbpassword = 'bV9!gQ8*';
+$dbname = 'sql2287835';
 // Create connection
-$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+$conn = new mysqli ($host, $dbusername, $dbname, $dbpassword);
 if (mysqli_connect_error()){
 die('Connect Error ('. mysqli_connect_errno() .') '
 . mysqli_connect_error());
 }
 else{
-$sql = "INSERT INTO account ('username', 'password')
+$sql = "INSERT INTO account (username, password)
 values ('$username','$password')";
 if ($conn->query($sql)){
 echo "New record is inserted sucessfully";
