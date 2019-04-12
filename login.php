c2202778fc41dd4b646d8b44ca393f758e668ef5
Pawel <pablogrosinsi@gmail.com>
2019-04-12 22:47:13 +0200

    .

--- login.php
+++ login.php
@@ -1,25 +1,38 @@
 <?php
-$login = filter_input(INPUT_POST,['login']);
-$password = filter_input(INPUT_POST,['password']);
-
-
-      if (!$login || !$password )
-      {
-        echo 'Brak wszystkich danych';
-        exit;
-      }
-      @ $conn = new mysqli('sql2.freesqldatabase.com','sql2287835','sql2287835','bV9!gQ8*');
-      
-      if (mysqli_connect_errno())
-      {
-        echo 'Połączenie z bazą nie powiodło się. Spóbuj ponownie';
-        exit;
-      }else
-      {
-      $sql = "INSERT INTO dane (id,email, pass)
-      VALUES ('', $login, $password)";
-      }
-   
-   $conn->close();
-
+ $username = filter_input(INPUT_POST, 'login');
+ $password = filter_input(INPUT_POST, 'pass');
+ if (!empty($username)){
+if (!empty($password)){
+$host = "sql2.freesqldatabase.com";
+$dbusername = "sql2287835";
+$dbpassword = "bV9!gQ8*";
+$dbname = "dane";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
+}
+else{
+$sql = "INSERT INTO account (email, pass)
+values ('$username','$password')";
+if ($conn->query($sql)){
+echo "New record is inserted sucessfully";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+}
+else{
+echo "Password should not be empty";
+die();
+}
+}
+else{
+echo "Username should not be empty";
+die();
+}
 ?>
