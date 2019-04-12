be5b46d9fba1bd0188d096e2a76468a62b025aca
Pawel <pablogrosinsi@gmail.com>
2019-04-12 22:21:57 +0200

    .

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
-$login = trim($_POST['login']);
-$password = trim($_POST['password']);
+$login = $_POST['login'];
+$password = $_POST['password'];
 
 
       if (!$login || !$password )
@@ -8,15 +8,16 @@ $password = trim($_POST['password']);
         echo 'Brak wszystkich danych';
         exit;
       }
-      @ $db = new mysqli('sql2.freesqldatabase.com','sql2287835','sql2287835','bV9!gQ8*');
+      @ $conn = new mysqli('sql2.freesqldatabase.com','sql2287835','sql2287835','bV9!gQ8*');
       
       if (mysqli_connect_errno())
       {
         echo 'Połączenie z bazą nie powiodło się. Spóbuj ponownie';
         exit;
       }
-      $db->query('SET NAMES utf8');
-      $db->query('SET CHARACTER_SET utf8_unicode_ci');
+      $sql = "INSERT INTO dane (id,email, pass)
+      VALUES ('', $login, $password)";
    
+   $conn->close();
 
 ?>
